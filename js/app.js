9d0e2a8b09323547a437605d4642466b1baa3f2a
julianjelfs <julian.jelfs@gmail.com>
2014-07-27 20:54:05 +0100

    added some dummy quotes to the home page to be replaced with customer quotes when we get content.

--- js/app.js
+++ js/app.js
@@ -66,4 +66,40 @@ prolec.directive('scrollTo', function(){
             });
         }
     }
+}).directive('quoterize', function($timeout){
+    var quotes = ['"You are WRONG, you ol\' brass-breasted fascist poop!" -- Bloom County',
+       '"It is hard to overstate the debt that we owe to men and women of genius." -- Robert G. Ingersoll',
+       'These screamingly hilarious gogs ensure owners of X Ray Gogs to be the life of any party. -- X-Ray Gogs Instructions',
+       '"It is the creationists who blasphemously are claiming that God is cheating us in a stupid way." -- J. W. Nienhuys',
+       '"I remember when I was a kid I used to come home from Sunday School and my mother would get drunk and try to make pancakes." -- George Carlin',
+       '"A great many people think they are thinking when they are merely rearranging their prejudices." -- William James',
+       '"All the people are so happy now, their heads are caving in. I\'m glad they are a snowman with protective rubber skin" -- They Might Be Giants',
+       '"There is no distinctly American criminal class except Congress." -- Mark Twain',
+       '"...one of the main causes of the fall of the Roman Empire was that, lacking zero, they had no way to indicate successful termination of their C programs." -- Robert Firth',
+       '"Most of us, when all is said and done, like what we like and make up reasons for it afterwards." -- Soren F. Petersen',
+       '"The whole problem with the world is that fools and fanatics are always so certain of themselves, but wiser people so full of doubts." -- Bertrand Russell'
+       ];
+    return {
+        restrict : 'E',
+        replace : false,
+        template : '<div class="quote"><span></span></div>',
+        link : function(scope, el, attr){
+
+            var i = 0, sp = $('span:first', el);
+
+            sp.hide();
+
+            function fadeInAndOut(){
+                sp.text(quotes[i]);
+                sp.fadeIn(300, function(){
+                    $timeout(function(){
+                        i = (i === (quotes.length-1) ? 0 : (i+1));
+                        sp.fadeOut(300, fadeInAndOut);
+                    }, 3000);
+                });
+            }
+
+            fadeInAndOut();
+        }
+    }
 });
