9fcba3d4a797cb915620fc9d8922f7966f7bc077
julianjelfs <julian.jelfs@gmail.com>
2014-09-03 21:25:46 +0100

    added some real quotes to the spinner. Tried to use the photos but the ones I have so far as pretty poor quality. Can't really use them.

--- js/app.js
+++ js/app.js
@@ -14,17 +14,12 @@ $(function(){
         }]
     });
 
-    var quotes = ['"You are WRONG, you ol\' brass-breasted fascist poop!" -- Bloom County',
-       '"It is hard to overstate the debt that we owe to men and women of genius." -- Robert G. Ingersoll',
-       'These screamingly hilarious gogs ensure owners of X Ray Gogs to be the life of any party. -- X-Ray Gogs Instructions',
-       '"It is the creationists who blasphemously are claiming that God is cheating us in a stupid way." -- J. W. Nienhuys',
-       '"I remember when I was a kid I used to come home from Sunday School and my mother would get drunk and try to make pancakes." -- George Carlin',
-       '"A great many people think they are thinking when they are merely rearranging their prejudices." -- William James',
-       '"All the people are so happy now, their heads are caving in. I\'m glad they are a snowman with protective rubber skin" -- They Might Be Giants',
-       '"There is no distinctly American criminal class except Congress." -- Mark Twain',
-       '"Most of us, when all is said and done, like what we like and make up reasons for it afterwards." -- Soren F. Petersen',
-       '"The whole problem with the world is that fools and fanatics are always so certain of themselves, but wiser people so full of doubts." -- Bertrand Russell'
-       ], i = 0, sp = $('span:first', 'div.quote'), nav = $('nav');
+    var quotes = ['Peter and the team were great throughout the build project',
+       'Not only was their work delivered on time to a high standard, but it was really the simple things they did well that stood out',
+       'The team added value with both cost saving and practical suggestions', 
+       'Nice guys who took care and pride in their work making us trust them',
+       'We would not hesitate to recommend them or to use them again for the next phase of our project'
+       ], i = 0, sp = $('div.quote-inner'), nav = $('nav');
 
     sp.hide();
 
@@ -34,7 +29,7 @@ $(function(){
             setTimeout(function(){
                 i = (i === (quotes.length-1) ? 0 : (i+1));
                 sp.fadeOut(300, fadeInAndOut);
-            }, 3000);
+            }, 4000);
         });
     }
 
